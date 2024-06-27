7f1232babce734a05534036a58bc55fcba0ee8d8
Uqzma <uqzma@student.kit.edu>
2024-06-27 10:53:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMembers() {
+    var members = [
+        {name: 'John', skills: ['javascript', 'html', 'css']},
+        {name: 'Alice', skills: ['java', 'spring', 'hibernate']},
+        {name: 'Bill', skills: ['python', 'django', 'flask']}
+    ];
+
+    return members;
+}
